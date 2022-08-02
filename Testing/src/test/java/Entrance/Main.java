268cf48a959ef28198e4eea23866bdc62f1b63e5
pntsunts <50695155+pntsunts@users.noreply.github.com>
2022-08-02 15:01:22 +0200

    With Extent Report

--- Testing/src/test/java/Entrance/Main.java
+++ Testing/src/test/java/Entrance/Main.java
@@ -8,9 +8,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
 import Pages.Selecting;
 
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 
 
 public class Main {
@@ -19,7 +28,32 @@ public class Main {
 	  String driverpath = "C:\\chromedriver.exe";
 	  public WebDriver driver;
 	  
-	  @BeforeTest 
+	  public ExtentHtmlReporter htmlReporter;
+	  public ExtentReports extent;
+	  public ExtentTest test;
+	  
+	  @BeforeTest
+	  public void Setextent() {
+		  
+		  htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "/test-output/Report.html");
+		  htmlReporter.config().setDocumentTitle("Automation Report");
+		  htmlReporter.config().setReportName("Functional Report");
+		  htmlReporter.config().setTheme(Theme.DARK);
+		  
+		  extent = new ExtentReports();
+		  
+		  extent.attachReporter(htmlReporter);
+		  
+		  extent.setSystemInfo("Hostname", "LocalHost");
+		  extent.setSystemInfo("OS", "Windows10");
+		  extent.setSystemInfo("Tester Name", "Peter");
+		  extent.setSystemInfo("Browser", "Chrome");
+		
+		  
+	  }
+	  
+	  
+	  @BeforeMethod 
 	  public void launch() throws InterruptedException {
 	    	
 	  System.setProperty("webdriver.chrome.driver", driverpath);
@@ -31,10 +65,12 @@ public class Main {
 	     
 	  driver.get(baseUrl);
 	  }
-	  
+	 
 	  @Test (priority = 0)
 	  
 	  public void career() throws IOException, InterruptedException {
+		  
+		  test = extent.createTest("career");
 		
 		  Selecting sele = new Selecting(driver);
 		  sele.career();
@@ -42,16 +78,39 @@ public class Main {
 		  sele.Job();
 	  } 
 	  
-	  @Test (priority = 1)
- 
-	  public void apply() {
-		  Selecting sele = new Selecting(driver);
-		  sele.apply();
-		  sele.name();
-		  sele.email();
-		  sele.phoneNumber();
-		  sele.submit();
+	  
+	  @AfterMethod 
+	  public void Teardown(ITestResult result) {
+		  
+		  if (result.getStatus() == ITestResult.FAILURE) {
+			  test.log(Status.FAIL, "TEST FAILED IS "+ result.getName());
+			  test.log(Status.FAIL, "TEST FAILED IS "+ result.getThrowable());
+		  }
+		  else if (result.getStatus() == ITestResult.SKIP) {
+			  test.log(Status.SKIP, "TEST CASE SKIPPED IS" + result.getName());
+		  }
+		  else if (result.getStatus() == ITestResult.SUCCESS) {
+			  test.log(Status.PASS, "TEST PASSED IS "+ result.getName());
+		  }
 	  }
+	  
+	  @AfterTest
+	  
+	  public void endReport() {
+		  
+		  extent.flush();
+	  }	
+	  
+//	  @Test (priority = 1)
+// 
+//	  public void apply() {
+//		  Selecting sele = new Selecting(driver);
+//		  sele.apply();
+//		  sele.name();
+//		  sele.email();
+//		  sele.phoneNumber();
+//		  sele.submit();
+//	  }
 	 
 
  }
